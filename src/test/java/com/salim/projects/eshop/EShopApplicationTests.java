b75a611a7ce109d5a1bc3959d4517795ed425e07
MS23 <mehdi.salim.23@gmail.com>
2024-11-02 12:31:01 +0100

    First Commit

---
+++ src/test/java/com/salim/projects/eshop/EShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salim.projects.eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
